182b015e1b6a6927cf73016b9af63c1b861027ac
Kashia Moua <kserenitym@gmail.com>
2021-08-12 16:42:44 -0500

    Updated styling on all components

--- src/components/UserPage/UserPage.jsx
+++ src/components/UserPage/UserPage.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import LogOutButton from '../LogOutButton/LogOutButton';
 import {useSelector} from 'react-redux';
+import './UserPage.css';
 
 function UserPage() {
   // this component doesn't do much to start, just renders some user reducer info to the DOM
@@ -8,7 +9,7 @@ function UserPage() {
   return (
     <div className="container">
       <h2>Welcome, {user.username}!</h2>
-      <p>Your ID is: {user.id}</p>
+      <p className='userInfo'>Your ID is: {user.id}</p>
       <LogOutButton className="btn" />
     </div>
   );
